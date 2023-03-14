160efea95115c12ce1c98729edc5c5691e65b26e
Bobkov Maxim <bobkov.maxim2018@yandex.ru>
2023-03-14 11:01:53 +0300

    new message box

---
+++ lab_2/lab_2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace lab_2
+{
+    public class Win32
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr MessageBox(int hWnd, String text,
+        String caption, uint type);
+    }
+}
